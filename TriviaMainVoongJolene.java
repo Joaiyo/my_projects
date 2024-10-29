078ae99742c697e93206b774bc3935bce822ff73
Jovo <jolenevoong@gmail.com>
2024-10-29 11:48:10 -0700

    Add files via upload

---
+++ TriviaMainVoongJolene.java
@@ -0,0 +1,13 @@
+/*
+ * Name: Jolene Voong
+ * Date: 12/6/2023
+ * Class: CSC 15 - Section 14
+ * Description: This program creates a simple trivia game that uses arrays to store the questions and answers.
+ */ 
+
+public class TriviaMainVoongJolene {
+    
+    public static void main(String[] args) {
+        TriviaHelperVoongJolene.run();
+    }
+}
